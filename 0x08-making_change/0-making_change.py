d751ae3d66e86e481cb43f43a03ea7b9c4dccbd3
jannelson36 <jannelson36@gmail.com>
2023-08-23 15:08:50 +0300

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module for makeChange method"""
+
+
+def makeChange(coins, total):
+    """Finds the minimum quantity of coins required to reach
+       a specified total value using a collection of coins with varying denominations.
+    """
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
